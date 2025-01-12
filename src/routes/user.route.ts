0bda1c8c8328ec7902248732f0aec131d19c42d7
Thiago Fantino <thiagofl20@hotmail.com>
2025-01-12 19:12:24 -0300

    Now you can select the stats of the day you want with the new get method

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -206,11 +206,85 @@ const UserRoute = (prisma: PrismaClient) => {
       res.status(500).json({ error: 'Error al obtener las estadísticas' });
     }
   });
+
+  router.get('/:id/statsByDate', async (req, res) => {
+    const { id } = req.params;
+    const { fecha } = req.query;  // Utilizamos un query parameter para recibir la fecha
   
+    try {
+      console.log(`Buscando usuario con ID: ${id}`);
+      const user = await prisma.user.findUnique({
+        where: { id: parseInt(id) },
+      });
   
+      if (!user) {
+        console.error(`Usuario con ID ${id} no encontrado`);
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+  
+      console.log(`Usuario encontrado: ${JSON.stringify(user)}`);
+  
+      // Obtener todas las estadísticas del usuario
+      const stats = await prisma.userStats.findMany({
+        where: {
+          userId: parseInt(id),
+        },
+        orderBy: {
+          fecha: 'asc',  // Ordenar por fecha
+        },
+      });
+  
+      // Agrupar las estadísticas por fecha (en formato local) y sumar los valores
+      const groupedStats = stats.reduce((acc, stat) => {
+        const statDate = new Date(stat.fecha).toLocaleDateString(); // Convertir la fecha a formato local
+        if (!acc[statDate]) {
+          acc[statDate] = {
+            fecha: statDate,
+            tiempo: 0,
+            entrenamientos: 0,
+            calorias: 0,
+          };
+        }
+        acc[statDate].tiempo += stat.tiempo;
+        acc[statDate].entrenamientos += stat.entrenamientos;
+        acc[statDate].calorias += stat.calorias;
+  
+        return acc;
+      }, {});
+  
+      // Convertir el objeto `groupedStats` a un array
+      const statsArray = Object.values(groupedStats);
+  
+      console.log(`Estadísticas agrupadas: ${JSON.stringify(statsArray)}`);
+  
+      // Si se pasa una fecha en los parámetros de la query, filtramos las estadísticas por fecha
+      if (fecha) {
+        // Filtrar las estadísticas por la fecha solicitada
+        const filteredStats = statsArray.filter(stat => stat.fecha === fecha);
+  
+        if (filteredStats.length === 0) {
+          return res.status(404).json({ message: `No se encontraron estadísticas para la fecha: ${fecha}.` });
+        }
+  
+        // Devolver las estadísticas filtradas
+        return res.json({ user, stats: filteredStats });
+      }
+  
+      // Si no se pasa una fecha, devolver todas las estadísticas
+      res.json({ user, stats: statsArray });
+    } catch (error) {
+      console.error('Error en la consulta:', error);
+      res.status(500).json({ error: 'Error al obtener las estadísticas' });
+    }
+  });
+  
+
+
+
+
+  
+
   return router;
 };
 
 export default UserRoute;
-
-
